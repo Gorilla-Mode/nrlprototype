90c9a0bedbe50ae606dcc8a3b046a8599c785a31
Gorilla-Mode <180402289+Gorilla-Mode@users.noreply.github.com>
2026-09-10 21:50:40 +0200

    feat: obstacle object

---
+++ src/lib/reporting/obstacle.ts
@@ -0,0 +1,10 @@
+﻿export type Obstacle =
+    {
+        id: string;
+        type: string;
+        description: string;
+        height: number;
+        gps_position: { lat: number; lng: number };
+        timestamp: Date;
+        obstacle_position: { lat: number; lng: number };
+    }
